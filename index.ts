2785fe1b31340046d069040f8d85cd92752e0064
Khanh Nguyen <khanh.nguyen@frosmo.com>
2021-01-28 22:47:50 +0200

    passport google auth route

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import authRoute from './routes/authRoutes';
 
 const app = express();
 const port = 5000;
+
+app.use('/auth', authRoute);
 
 app.listen(port, () => console.log(`Running on port ${port}`));
---
+++ routes/authRoutes.ts
@@ -0,0 +1,34 @@
+import passport from 'passport';
+import { Router, Request, Response } from 'express';
+
+const authRoute = Router();
+authRoute.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+authRoute.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req: Request, res: Response) => {
+        res.redirect('/sunny');
+    }
+);
+
+
+authRoute.get(
+    '/api/current_user', (req: Request, res: Response) => {
+        res.send(req.user);
+    }
+);
+
+authRoute.get(
+    '/api/logout', (req: Request, res: Response) => {
+        req.logout();
+        res.redirect('/');
+    }
+);
+
+export default authRoute;
