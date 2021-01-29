9936c0f26c452654d9ea1f3de2e64c90e20d2117
Khanh Nguyen <khanh.nguyen@frosmo.com>
2021-01-29 17:44:43 +0200

    Google auth done

--- services/passport.ts
+++ services/passport.ts
@@ -1,11 +1,18 @@
 import { Application } from 'express';
 import passport from 'passport';
+import cookieSession from 'cookie-session';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import config from '../config';
 import User from '../models/User';
 
 class Passport {
   public init(app: Application): Application {
+    app.use(
+      cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30days
+        keys: [config.cookieKey] // a random key to encrypt our cookie
+      })
+    );
     app = app.use(passport.initialize());
     app = app.use(passport.session());
 
@@ -30,12 +37,17 @@ class Passport {
         },
         async (accessToken, refreshToken, profile, done) => {
           const existingUser = await User.findOne({ googleId: profile.id })
-
+          console.log('existingUser');
+          console.log(existingUser);
           if (existingUser) {
+            console.log('existing user');
+            console.log(existingUser);
             return done(null, existingUser);
           }
 
           const user = await new User({ googleId: profile.id }).save()
+
+          console.log('new user');
           done(null, user);
         }
       )
