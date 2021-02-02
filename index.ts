f98950afcbdea718cd4d4761f784f99ee1efa980
Khanh Nguyen <khanh.nguyen@frosmo.com>
2021-02-02 04:51:45 +0200

    Heroku build script

--- index.ts
+++ index.ts
@@ -4,10 +4,13 @@ import weatherRoutes from './routes/weatherRoutes';
 import followRoutes from './routes/followRoutes';
 import Passport from './services/passport';
 import Mongoose from './services/mongoose';
+import { preProcessFile } from 'typescript';
 
-require('dotenv').config();
-
+if (process.env.NODE_ENV === 'development') {
+    require('dotenv').config();
+}
 const app = express();
+
 let port = process.env.PORT || 5000;
 
 Mongoose.init();
@@ -16,5 +19,15 @@ Passport.init(app);
 app.use('/', authRoutes);
 app.use('/', weatherRoutes);
 app.use('/', followRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve production assets
+    app.use(express.static('client/build'))
+    // Express will serve index.html if route not recognized
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(port, () => console.log(`Running on port ${port}`));
